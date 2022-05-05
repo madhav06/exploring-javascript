1a7733834136ec2bd6c7a5f3bb4e96ea0203e192
madhav06 <mnandan06@gmail.com>
2022-05-06 02:24:59 +0530

    --arrays , fix prettier issues

---
+++ arrays/lastElement.js
@@ -0,0 +1,22 @@
+// Get last app
+
+/*
+Complete the function getLastApp such that it returns the last,
+ element from the apps array it receives as a parameter.
+
+*/
+
+/**
+ * @param {array} apps
+ */
+/**
+ * @param {array} apps
+ */
+ function getLastApp(apps) {
+    return apps[apps.length - 1];
+}
+
+// Sample usage - do not modify
+console.log(getLastApp(["Chrome", "Clock", "Twitter"])); // "Twitter"
+console.log(getLastApp(["Safari", "Contacts"])); // "Contacts"
+
