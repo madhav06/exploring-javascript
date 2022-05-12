842494c74070f8375cad8be77c59ca375be11553
madhav06 <mnandan06@gmail.com>
2022-05-12 05:48:57 +0530

    bind(): a borrower method

---
+++ advanced-methods/bind.js
@@ -0,0 +1,20 @@
+// // Using bind() to borrow methods from a different object
+
+// Suppose you have a runner object that has the run() method:
+
+let runner = {
+    name: 'Runner',
+    run: function(speed) {
+        console.log(this.name + ' runs at ' + speed + ' mph.');
+    }
+};
+
+let flyer = {
+    name: 'Flyer',
+    fly: function(speed) {
+        console.log(this.name + ' flies at ' + speed + ' mph.');
+    }
+};
+
+let run = runner.run.bind(flyer, 20);
+run();
