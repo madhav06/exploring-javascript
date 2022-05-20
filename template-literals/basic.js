5d174bfd6318d06fee7a5a2da4b6d48baa22b3ca
madhav06 <mnandan06@gmail.com>
2022-05-21 01:00:13 +0530

    --basics of template literals

---
+++ template-literals/basic.js
@@ -0,0 +1,14 @@
+// basic syntax
+
+let str = `Template literal in ES6`
+
+console.log(str);
+console.log(str.length);
+console.log(typeof str);
+
+let anotherStr = `Here is another template literal`
+
+let strWithBacktick = `Template literals use backticks \ instead`
+
+let msg = ['This text', 'can', 'span multiple lines'].join('\n');
+
