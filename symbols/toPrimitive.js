ad5dfe0b27e2c532fe9c936ab7f5672378124867
madhav06 <mnandan06@gmail.com>
2022-05-24 06:17:45 +0530

    -- using Symbol.toPrimitive method

---
+++ symbols/toPrimitive.js
@@ -0,0 +1,29 @@
+// Here is an example of using the Symbol.toPrimitive method.
+
+function Money(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+}
+
+Money.prototype[Symbol.toPrimitive] = function(hint) {
+    var result;
+
+    switch (hint) {
+        case 'string':
+            result = this.amount + this.currency;
+            break;
+        case 'number':
+            result = this.amount;
+            break;
+        case 'default':
+            result = this.amount + this.currency;
+            break;
+    }
+    return result;
+}
+
+var price = new Money(799, 'USD');
+
+console.log('Price is ' + price); // Price is 799USD
+console.log(price + 1); // 800
+console.log(String(price)); // 799USD
