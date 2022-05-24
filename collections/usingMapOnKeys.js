15965c8f45a488061ef23ea699e745e30aafb283
madhav06 <mnandan06@gmail.com>
2022-05-24 08:55:21 +0530

    -- Iterate over map keys

---
+++ collections/usingMapOnKeys.js
@@ -0,0 +1,21 @@
+// Iterate over map keys
+
+let john = {
+    name: 'John Doe'
+},
+lily = {
+    name: 'Lily Bush'
+},
+peter = {
+    name: 'Peter Drucker'
+};
+
+let userRoles = new Map([
+    [john, 'admin'],
+    [lily, 'editor'],
+    [peter, 'subscriber']
+]);
+
+for (const user of userRoles.keys()) {
+    console.log(user.name);
+}
