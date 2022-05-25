c6d42699a41955b73b7415e141a94f5b350a3380
madhav06 <mnandan06@gmail.com>
2022-05-25 13:29:37 +0530

    --Proxy, proxy object

---
+++ proxy-reflection/simpleProxyExample.js
@@ -0,0 +1,16 @@
+// A simple proxy example
+
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com'
+}
+
+const handler = {
+    get(target, property) {
+        console.log(`Property ${property} has been read.`);
+        return target[property];
+    }
+}
+
+const proxyUser = new Proxy(user, handler);
